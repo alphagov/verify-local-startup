6704cd4f2e91a2f0e1812aae663dfaa165d6c25e
Andy Paine <andy.paine@outlook.com>
2017-10-30 14:06:36 +0000

    BAU: Direct environment output to stdout and add help option to configure each app
    Authors: @andy-paine

--- generate-env.sh
+++ generate-env.sh
@@ -1,12 +1,22 @@
 #!/usr/bin/env bash
-set -ue
+set -e
+
+if [ "$1" == "-h" ] || [ "$1" == "--help" ] ; then
+    echo "Generates the required environment variables for running verify apps"
+    echo "Run ./generate-env.sh > \$app_directory/local.env to configure each app" 
+    exit 0
+fi
 
 if test ! -d data; then
   command -v cfssl || brew install cfssl
   ./generate/hub-dev-pki.sh
 fi
 
-cat <<EOF > ../ida-msa/local.env
+### PORTS
+cat config/env.sh
+
+### MSA
+cat <<EOF
 export HUB_TRUST_STORE=$(base64 data/pki/hub.ts)
 export METADATA_TRUST_STORE=$(base64 data/pki/metadata.ts)
 export MSA_SIGNING_KEY_PRIMARY=$(base64 data/pki/sample_rp_msa_signing_primary.pk8)
@@ -16,4 +26,3 @@ export MSA_SIGNING_CERT_SECONDARY=$(base64 data/pki/sample_rp_msa_signing_second
 export MSA_ENCRYPTION_KEY_PRIMARY=$(base64 data/pki/sample_rp_msa_encryption_primary.pk8)
 export MSA_ENCRYPTION_CERT_PRIMARY=$(base64 data/pki/sample_rp_msa_encryption_primary.crt)
 EOF
-cat config/env.sh >> ../ida-msa/local.env
