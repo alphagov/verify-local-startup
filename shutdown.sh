24092a21e93b56dcde3dff1d9b9fb0bd99e4c40b
Rachael Booth <rachael.booth@digital.cabinet-office.gov.uk>
2017-11-14 13:49:21 +0000

    BAU: Update shutdown.sh to stop verify service provider and matching service adapter
    Adds verify service provider to services to be shut down
    Corrects name of msa
    
    Authors: @rachaelbooth

--- shutdown.sh
+++ shutdown.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-services=${@:-"config stub-event-sink policy saml-engine saml-proxy saml-soap-proxy msa test-rp stub-idp ida-frontend"}
+services=${@:-"config stub-event-sink policy saml-engine saml-proxy saml-soap-proxy verify-matching-service-adapter test-rp stub-idp ida-frontend verify-service-provider-*"}
 
 for service in $services; do
   pkill -9 -f "${service}.jar"
