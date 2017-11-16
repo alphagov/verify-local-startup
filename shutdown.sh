530157b37a43caddb05aa8e683c5309820a91ebb
Rachael Booth <rachael.booth@digital.cabinet-office.gov.uk>
2017-11-16 17:09:46 +0000

    BAU: Improve shutdown script
    Shutdown script now also kills frontend, the metadata server, oscp_responder, and remaining startup related scripts
    
    Authors: @rachaelbooth

--- shutdown.sh
+++ shutdown.sh
@@ -1,10 +1,15 @@
 #!/usr/bin/env bash
 
-services=${@:-"config stub-event-sink policy saml-engine saml-proxy saml-soap-proxy verify-matching-service-adapter test-rp stub-idp ida-frontend verify-service-provider-*"}
+services=${@:-"config stub-event-sink policy saml-engine saml-proxy saml-soap-proxy verify-matching-service-adapter test-rp stub-idp ida-frontend frontend verify-service-provider-*"}
 
 for service in $services; do
   pkill -9 -f "${service}.jar"
 done
+
+pkill -9 verify_metadata_server
+pkill -9 ./vsp-startup.sh
+pkill -9 -f ocsp_responses
+pkill -9 -f bin/ocsp_responder
 
 exit 0
 
