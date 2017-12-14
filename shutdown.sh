8b3d6aaf35beeb7edecbb454bbb90ddc679bacf3
Andy Paine <andy.paine@outlook.com>
2017-12-14 14:00:35 +0000

    Add vsp back to shutdown and remove ida-frontend

--- shutdown.sh
+++ shutdown.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-services=${@:-"config stub-event-sink policy saml-engine saml-proxy saml-soap-proxy verify-matching-service-adapter test-rp ida-sample-rp stub-idp ida-frontend frontend"} 
+services=${@:-"config stub-event-sink policy saml-engine saml-proxy saml-soap-proxy verify-matching-service-adapter test-rp ida-sample-rp stub-idp frontend verify-service-provider-*"} 
 
 for service in $services; do
   pkill -9 -f "${service}.jar"
