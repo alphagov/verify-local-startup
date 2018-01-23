cf5c580672126807ffabb7bd63320c6160ddf62f
Karol Gancarz <karol.gancarz@digital.cabinet-office.gov.uk>
2018-01-23 15:53:20 +0000

    Firebreak: Dockerise hub components
    Revert changes to files in the config folder, which are not used by Docker and are required to run tests and pre-commit scripts from other projects
    
    Author: @CharlesIC

--- config/env.sh
+++ config/env.sh
@@ -1,5 +1,7 @@
+#!/usr/bin/env bash
+
 export METADATA_PORT=55500
-export METADATA_URL=http://localhost:55500/dev.xml
+export METADATA_URL=http://localhost:${METADATA_PORT}/dev.xml
 
 export POLICY_PORT=50110
 export CONFIG_PORT=50240
@@ -9,14 +11,14 @@ export EVENT_SINK_PORT=51100
 export SAML_ENGINE_PORT=50120
 export TEST_RP_MSA_PORT=50210
 export TEST_RP_PORT=50130
-export STUB_IDP_PORT=50140
 export VERIFY_FRONTEND_API_PORT=50190
 export COMPLIANCE_TOOL_PORT=50270
-
 export VSP_PORT=50400
-
 export FRONTEND_PORT=50300
-export FRONTEND_URI=http://localhost:90
-export STUB_IDP_URI=http://localhost:92
-export TEST_RP_URI=http://localhost:94
+export STUB_IDP_PORT=50140
+
+export FRONTEND_URI=http://localhost:${FRONTEND_PORT}
+export STUB_IDP_URI=http://localhost:${STUB_IDP_PORT}
+export TEST_RP_URI=http://localhost:${TEST_RP_PORT}
+
 export MSA_URI=http://msa
