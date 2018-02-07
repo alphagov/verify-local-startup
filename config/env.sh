6d815e57c0f7c0b6b5e3954d187749644f461d71
Karol Gancarz <karol.gancarz@digital.cabinet-office.gov.uk>
2018-02-07 03:45:31 +0000

    BAU: Fix unbound variable error in Hub acceptance tests

--- config/env.sh
+++ config/env.sh
@@ -2,13 +2,13 @@
 
 script_location=$(dirname "${BASH_SOURCE[0]}")
 ports_file="$script_location/ports.env"
+vls_type=${VLS_TYPE:-}
 
 set -o allexport
 source $ports_file
 set +o allexport
-if [ "$VLS_TYPE" = "localhost" ]; then
+if [ "$vls_type" = "localhost" ]; then
     export MSA_URI=http://localhost:${TEST_RP_MSA_PORT}
 else
     export MSA_URI=http://msa
 fi
-
