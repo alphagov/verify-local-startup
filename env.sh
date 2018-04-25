f7593580815baf27ae363434aee7a8e7a95e539e
Andy Paine <andy.paine@digital.cabinet-office.gov.uk>
2018-04-25 11:59:38 +0100

    BAU: Fix up test-rp and move some common env vars into docker-compose
    Test-rp has added some extra config that needs to be in local
    startup too

--- env.sh
+++ env.sh
@@ -13,31 +13,30 @@ HUB_SIGNING_CERT=$($BASE64 data/pki/hub_signing_primary.crt)
 EOF
 
 cat << EOF > test-rp.env
+KEY_TYPE=encoded
+CERT_TYPE=encoded
 TEST_RP_SIGNING_KEY=$($BASE64 data/pki/sample_rp_signing_primary.pk8)
 TEST_RP_SIGNING_CERT=$($BASE64 data/pki/sample_rp_signing_primary.crt)
 TEST_RP_ENCRYPTION_KEY=$($BASE64 data/pki/sample_rp_encryption_primary.pk8)
 TEST_RP_ENCRYPTION_CERT=$($BASE64 data/pki/sample_rp_encryption_primary.crt)
-TEST_RP_MSA_SIGNING_KEY=$($BASE64 data/pki/sample_rp_msa_signing_primary.pk8)
-TEST_RP_MSA_SIGNING_CERT=$($BASE64 data/pki/sample_rp_msa_signing_primary.crt)
-TEST_RP_MSA_ENCRYPTION_KEY=$($BASE64 data/pki/sample_rp_msa_encryption_primary.pk8)
-TEST_RP_MSA_ENCRYPTION_CERT=$($BASE64 data/pki/sample_rp_msa_encryption_primary.crt)
-HUB_TRUSTSTORE=$($BASE64 data/pki/hub.ts)
-HUB_TRUSTSTORE_PASSWORD=marshmallow
+TRUSTSTORE_TYPE=encoded
+TRUSTSTORE=$($BASE64 data/pki/hub.ts)
+TRUSTSTORE_PASSWORD=marshmallow
 EOF
 
+#TEST_RP_MSA_SIGNING_KEY=$($BASE64 data/pki/sample_rp_msa_signing_primary.pk8)
+#TEST_RP_MSA_SIGNING_CERT=$($BASE64 data/pki/sample_rp_msa_signing_primary.crt)
+#TEST_RP_MSA_ENCRYPTION_KEY=$($BASE64 data/pki/sample_rp_msa_encryption_primary.pk8)
+#TEST_RP_MSA_ENCRYPTION_CERT=$($BASE64 data/pki/sample_rp_msa_encryption_primary.crt)
+
 cat << EOF > stub-idp.env
-PORT=80
 KEY_TYPE=encoded
-STUB_IDP_SIGNING_PRIVATE_KEY=$($BASE64 data/pki/stub_idp_signing_primary.pk8)
 CERT_TYPE=encoded
+STUB_IDP_SIGNING_PRIVATE_KEY=$($BASE64 data/pki/stub_idp_signing_primary.pk8)
 STUB_IDP_SIGNING_CERT=$($BASE64 data/pki/stub_idp_signing_primary.crt)
-STUB_IDP_BASIC_AUTH=false
-STUB_IDPS_FILE_PATH=/idps/stub-idps.yml
-METADATA_ENTITY_ID=https://dev-hub.local
+STUB_COUNTRY_SIGNING_PRIVATE_KEY=$($BASE64 data/pki/stub_idp_signing_primary.pk8)
+STUB_COUNTRY_SIGNING_CERT=$($BASE64 data/pki/stub_idp_signing_primary.crt)
 TRUSTSTORE_TYPE=encoded
 METADATA_TRUSTSTORE=$($BASE64 data/pki/metadata.ts)
 TRUSTSTORE_PASSWORD=marshmallow
-STUB_COUNTRY_SIGNING_PRIVATE_KEY=$($BASE64 data/pki/stub_idp_signing_primary.pk8)
-STUB_COUNTRY_SIGNING_CERT=$($BASE64 data/pki/stub_idp_signing_primary.crt)
-LOG_PATH=/app/logs
 EOF
