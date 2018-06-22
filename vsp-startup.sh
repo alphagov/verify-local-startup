df94f01616cb9a70d3dff1dfc0b6e1dd093a2c23
William Palmer <william.palmer@digital.cabinet-office.gov.uk>
2018-06-22 22:28:58 +0100

    vsp-msa fixes

--- vsp-startup.sh
+++ vsp-startup.sh
@@ -16,7 +16,9 @@ export SAML_SIGNING_KEY="$($BASE64 data/pki/sample_rp_signing_primary.pk8)"
 export SAML_PRIMARY_ENCRYPTION_KEY="$($BASE64 data/pki/sample_rp_encryption_primary.pk8)"
 export SERVICE_ENTITY_IDS='["http://vsp.dev-rp.local/SAML2/MD"]'
 export METADATA_TRUST_STORE="$($BASE64 data/pki/metadata.ts)"
-
+export TEST_RP_MSA_PORT=3300
+export IDA_TESTRP_MSA_ENTITYID="http://vsp.dev-rp-ms.local/SAML2/MD"
+ 
 lsof -ti:$VSP_PORT | xargs kill
 
 ./../verify-service-provider/build/install/verify-service-provider/bin/verify-service-provider server ../verify-service-provider/local-running/local-config.yml > logs/verify-service-provider_console.log &
