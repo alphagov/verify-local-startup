ceda4963027c400898a81614e6f2eff7ccaae04a
Andy Paine <andy.paine@digital.cabinet-office.gov.uk>
2018-07-29 19:44:30 +0100

    BAU: Modify metadata sources generator to allow importing
    - As we migrate towards using Ruby to generate more parts of
    the local startup environment, it would be better to be
    able to 'require' other Ruby scripts rather than have to call
    them via command line
    - To allow backwards compat, this modifies the script to use a
    module and calls the module if the script is executed directly

--- generate/metadata-sources.rb
+++ generate/metadata-sources.rb
@@ -10,51 +10,70 @@ def inline_cert(cert)
   IO.readlines(cert).map(&:strip).reject(&:empty?)[1 ... -1].join
 end
 
-if ARGV.size < 4
-  puts "Usage: metadata-sources.rb hub_signing_cert hub_encryption_cert idp_signing_cert output_dir"
-  exit 1
+module Metadata
+  module_function
+  def generate_metadata_sources(hub_signing_cert,
+                                hub_encryption_cert,
+                                idp_signing_cert,
+                                output_dir,
+                                frontend_uri,
+                                stub_idp_uri)
+    idps = {
+      'Stub IDP One' => 'stub-idp-one',
+      'Stub IDP Two' => 'stub-idp-two',
+      'Stub IDP Three' => 'stub-idp-three',
+      'Stub IDP Four' => 'stub-idp-four'
+    }
+
+    hub_yaml = {
+      'id' => 'VERIFY-HUB',
+      'entity_id' => 'https://dev-hub.local',
+      'assertion_consumer_service_uri' => "#{frontend_uri}/SAML2/SSO/Response/POST",
+      'organization' => { 'name' => 'Hub', 'url' => 'http://localhost', 'display_name' => 'Hub' },
+      'signing_certificates' => [
+        { 'name' => 'signing_primary', 'x509' => block_cert(hub_signing_cert) }
+      ],
+      'encryption_certificate' => { 'name' => 'encryption', 'x509' => block_cert(hub_encryption_cert) }
+    }
+
+    idp_yaml = {
+      'enabled' => true,
+      'signing_certificates' => [
+        { 'name' => 'signing_primary', 'x509' => inline_cert(idp_signing_cert) }
+      ],
+    }
+
+    Dir::chdir(output_dir) do
+      File.open('hub.yml', 'w') { |f| f.write(YAML.dump(hub_yaml)) }
+
+      idps.each do |name, id|
+        yaml = idp_yaml.update(
+          'organization' => { 'name' => id, 'url' => "http://#{id}.local", 'display_name' => name},
+          'entity_id' => "http://#{id}.local/SSO/POST",
+          'sso_uri' => "#{stub_idp_uri}/#{id}/SAML2/SSO",
+          'id' => id
+        )
+        File.open(File.join('idps', "#{id}.yml"), 'w') { |f| f.write(YAML.dump(yaml)) }
+      end
+    end
+  end
 end
 
-hub_signing_cert = ARGV[0]
-hub_encryption_cert = ARGV[1]
-idp_signing_cert = ARGV[2]
-output_dir = ARGV[3]
-
-idps = {
-  'Stub IDP One' => 'stub-idp-one',
-  'Stub IDP Two' => 'stub-idp-two',
-  'Stub IDP Three' => 'stub-idp-three',
-  'Stub IDP Four' => 'stub-idp-four'
-}
-
-hub_yaml = {
-  'id' => 'VERIFY-HUB',
-  'entity_id' => 'https://dev-hub.local',
-  'assertion_consumer_service_uri' => "#{ENV.fetch('FRONTEND_URI')}/SAML2/SSO/Response/POST",
-  'organization' => { 'name' => 'Hub', 'url' => 'http://localhost', 'display_name' => 'Hub' },
-  'signing_certificates' => [
-    { 'name' => 'signing_primary', 'x509' => block_cert(hub_signing_cert) }
-  ],
-  'encryption_certificate' => { 'name' => 'encryption', 'x509' => block_cert(hub_encryption_cert) }
-}
-
-idp_yaml = {
-  'enabled' => true,
-  'signing_certificates' => [
-    { 'name' => 'signing_primary', 'x509' => inline_cert(idp_signing_cert) }
-  ],
-}
-
-Dir::chdir(output_dir) do
-  File.open('hub.yml', 'w') { |f| f.write(YAML.dump(hub_yaml)) }
-
-  idps.each do |name, id|
-    yaml = idp_yaml.update(
-      'organization' => { 'name' => id, 'url' => "http://#{id}.local", 'display_name' => name},
-      'entity_id' => "http://#{id}.local/SSO/POST",
-      'sso_uri' => "#{ENV.fetch('STUB_IDP_URI')}/#{id}/SAML2/SSO",
-      'id' => id
-    )
-    File.open(File.join('idps', "#{id}.yml"), 'w') { |f| f.write(YAML.dump(yaml)) }
+if __FILE__ == $0 then
+  if ARGV.size < 4
+    puts "Usage: metadata-sources.rb hub_signing_cert hub_encryption_cert idp_signing_cert output_dir"
+    exit 1
   end
+
+  hub_signing_cert = ARGV[0]
+  hub_encryption_cert = ARGV[1]
+  idp_signing_cert = ARGV[2]
+  output_dir = ARGV[3]
+
+  Metadata.generate_metadata_sources(hub_signing_cert,
+                                     hub_encryption_cert,
+                                     idp_signing_cert,
+                                     output_dir,
+                                     ENV.fetch('FRONTEND_URI'),
+                                     ENV.fetch('STUB_IDP_URI'))
 end
