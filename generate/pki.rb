f1a3c2afc4eb8feb69224617d193115a70c6a172
Andy Paine <andy.paine@digital.cabinet-office.gov.uk>
2018-08-10 23:23:04 +0100

    BAU: Improve script
    - Use Open3 and raise errors for debugging purposes
    - Bump Gemfile.lock to use newer metadata generator

--- generate/pki.rb
+++ generate/pki.rb
@@ -3,6 +3,7 @@
 require 'yaml'
 require 'json'
 require 'fileutils'
+require 'open3'
 
 SCRIPT_DIR = File.expand_path(File.dirname(__FILE__))
 pki_to_generate = YAML.load_file(ARGV[0] ||= "#{SCRIPT_DIR}/pki.yml")
@@ -10,31 +11,35 @@ pki_dir = 'pki'
 FileUtils::mkdir_p pki_dir
 Dir.chdir pki_dir
 
-def get_template_csr(common_name)
-  {
-      "CN": common_name,
-      "hosts": [ "localhost" ],
-      "key": {
-          "algo": "rsa",
-          "size": 2048
-      },
-      "names": [
-          {
-              "C": "GB",
-              "ST": "London",
-              "L": "London",
-              "O": "Cabinet Office",
-              "OU": "GDS"
-          }
-      ]
-  }.to_json
+def csr_json()
+  '{"names": [ { "C": "GB", "ST": "London", "L": "London", "O": "Cabinet Office", "OU": "GDS" } ]}'
 end
 
 def generate_cert(type, common_name, file, ca_cert, ca_key)
-  output = %x(echo '#{get_template_csr(common_name)}' | \
-              cfssl gencert -config #{SCRIPT_DIR}/cfssl-config.json -profile "#{type}" -ca #{ca_cert} -ca-key #{ca_key} -loglevel=2 - | \
-              cfssljson -bare #{file})
-  raise output unless $?.success?
+  gencert_out, gencert_err, gencert_status = Open3.capture3(
+    'cfssl', 'gencert',
+    '-config', "#{SCRIPT_DIR}/cfssl-config.json",
+    '-cn', common_name,
+    '-ca', ca_cert,
+    '-ca-key', ca_key,
+    '-profile', type,
+    '-hostname', 'localhost',
+    '-loglevel=2', '-',
+    :stdin_data => csr_json
+  )
+
+  raise gencert_err unless gencert_status.success?
+  cfssl_json(gencert_out, file)
+end
+
+def cfssl_json(input, file)
+  write_out, write_status = Open3.capture2e(
+    'cfssljson',
+    '-bare', file,
+    stdin_data: input
+  )
+
+  raise write_out unless write_status.success?
 end
 
 def convert_key_cert(file)
@@ -46,8 +51,10 @@ end
 
 pki_to_generate['pki'].each do |ca_name, ca|
   puts "Generating Root CA: #{ca_name}"
-  output = %x(echo '#{get_template_csr(ca['common-name'])}' | cfssl genkey -initca -loglevel=2 - | cfssljson -bare #{ca['cert-file']})
-  raise output unless $?.success?
+  genkey_out, genkey_err, genkey_status = Open3.capture3('cfssl', 'genkey', '-initca', '-loglevel=2', '-cn', ca['common-name'], '-', :stdin_data => csr_json)
+  raise genkey_err unless genkey_status.success?
+
+  cfssl_json(genkey_out, ca['cert-file'])
 
   ca['intermediates'].each do |inter_name, inter|
     # Need to change values in the hash so that future access also gets these defaults
@@ -55,21 +62,15 @@ pki_to_generate['pki'].each do |ca_name, ca|
     inter['cert-file'] = inter.fetch('cert-file', "#{ca_name}-#{inter_name}")
 
     puts "\tGenerating Intermediate CA: #{inter_name}"
-    generate_cert('intermediate',
-                  inter['common-name'],
-                  inter['cert-file'],
-                  "#{ca['cert-file']}.pem",
-                  "#{ca['cert-file']}-key.pem")
+    generate_cert('intermediate', inter['common-name'], inter['cert-file'], "#{ca['cert-file']}.pem", "#{ca['cert-file']}-key.pem")
 
     inter['certs'].each do |cert_name, cert|
       cert_file = cert.fetch('cert-file', cert_name)
       common_name = cert.fetch('common-name', "Verify Local Startup #{cert_name.split("-").map{ |s| s.capitalize}.join(' ')}")
+
       puts "\t\tGenerating cert: #{cert_name}"
-      generate_cert(cert['type'],
-                    common_name,
-                    cert_file,
-                    "#{inter['cert-file']}.pem",
-                    "#{inter['cert-file']}-key.pem")
+      generate_cert(cert['type'], common_name, cert_file, "#{inter['cert-file']}.pem", "#{inter['cert-file']}-key.pem")
+
       convert_key_cert cert_file
     end
     convert_key_cert inter['cert-file']
