8686d46d58cfbbdaabdaa7cbfa75d7613cb98c25
Andy Paine <andy.paine@digital.cabinet-office.gov.uk>
2018-09-04 19:24:55 +0100

    BAU: Move .env files to configuration
    - These were making the root a bit dirty so move them to configuration
      with frontend.env
    - We weren't using hub.env so remove that too

--- env.sh
+++ env.sh
@@ -5,14 +5,7 @@ if [ "$(uname)" = "Darwin" ]; then
     BASE64="base64 -b0" # macOS
 fi
 
-cat << EOF > hub.env
-HUB_ENCRYPTION_KEY=$($BASE64 data/pki/hub_encryption_primary.pk8)
-HUB_ENCRYPTION_CERT=$($BASE64 data/pki/hub_encryption_primary.crt)
-HUB_SIGNING_KEY=$($BASE64 data/pki/hub_signing_primary.pk8)
-HUB_SIGNING_CERT=$($BASE64 data/pki/hub_signing_primary.crt)
-EOF
-
-cat << EOF > test-rp.env
+cat << EOF > configuration/test-rp.env
 KEY_TYPE=encoded
 CERT_TYPE=encoded
 TEST_RP_SIGNING_KEY=$($BASE64 data/pki/sample_rp_signing_primary.pk8)
@@ -24,12 +17,7 @@ TRUSTSTORE=$($BASE64 data/pki/hub.ts)
 TRUSTSTORE_PASSWORD=marshmallow
 EOF
 
-#TEST_RP_MSA_SIGNING_KEY=$($BASE64 data/pki/sample_rp_msa_signing_primary.pk8)
-#TEST_RP_MSA_SIGNING_CERT=$($BASE64 data/pki/sample_rp_msa_signing_primary.crt)
-#TEST_RP_MSA_ENCRYPTION_KEY=$($BASE64 data/pki/sample_rp_msa_encryption_primary.pk8)
-#TEST_RP_MSA_ENCRYPTION_CERT=$($BASE64 data/pki/sample_rp_msa_encryption_primary.crt)
-
-cat << EOF > stub-idp.env
+cat << EOF > configuration/stub-idp.env
 KEY_TYPE=encoded
 CERT_TYPE=encoded
 STUB_IDP_SIGNING_PRIVATE_KEY=$($BASE64 data/pki/stub_idp_signing_primary.pk8)
