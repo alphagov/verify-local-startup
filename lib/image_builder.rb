ce262f6d10a122166e526294de0032477a3a429c
Chris Wynne <christopher.wynne@digital.cabinet-office.gov.uk>
2021-05-20 17:38:35 +0100

    BAU: Allow using SNAPSHOTS in maven local
    Often when developing the hub we want to use a snapshot of another
    dependency, which has been published to your local maven repo.
    
    This change makes your local maven repo available in the docker builds.
    It copies the repo with a namespace to avoid issues with multiple
    threads.
    
    It adds `COPY` lines to the Dockerfile on the fly, rather than having to
    maintain a separate Dockerfile.

--- lib/image_builder.rb
+++ lib/image_builder.rb
@@ -2,6 +2,9 @@
 require 'os'
 require 'tty-spinner'
 require 'colorize'
+require 'fileutils'
+require 'tempfile'
+require 'securerandom'
 
 # This is our worker thread which builds our docker images
 class ImageBuilder
@@ -19,22 +22,33 @@ class ImageBuilder
       @write_success_log = write_success_log
       @output = "Starting build of #{repo_name}...\n"
     end
-  
+
     def build_image
       image_name = "#{@repo_name}:local"
       build_args = @config.fetch('build-args', []).map { |ba| "--build-arg #{ba.keys[0]}=#{ba.values[0]}" }.join " "
+      random_namespace = SecureRandom.hex # To prevent race conditions with separate threads
+      temp_m2_dirname = "m2_#{random_namespace}"
+      temp_dockerfile = "Dockerfile_#{random_namespace}"
+      dockerfile_content = insert_maven_local_copy_in_dockerfile(
+        "../#{@config['context']}/#{@config.fetch('dockerfile', 'Dockerfile')}",
+        temp_m2_dirname
+      )
+      File.open("../#{@config['context']}/#{temp_dockerfile}", 'w') { |f| f.write dockerfile_content }
+      FileUtils.cp_r "#{`realpath ~`.strip}/.m2/repository/uk/gov/ida/.", "../#{@config['context']}/#{temp_m2_dirname}"
       cmd = "docker build #{build_args}\
           --build-arg release=#{@release}\
           ../#{@config['context']}\
-          -f ../#{@config['context']}/#{@config.fetch('dockerfile', 'Dockerfile')}\
+          -f ../#{@config['context']}/#{temp_dockerfile}\
           -t #{image_name}\
           2>&1"
       output = `#{cmd}`
+      FileUtils.rm_rf("../#{@config['context']}/#{temp_m2_dirname}")
+      FileUtils.rm_rf("../#{@config['context']}/#{temp_dockerfile}")
       if $?.success?
         if @write_success_log
           @spinner.success(" - see #{@script_dir}/logs/#{@repo_name}_build.log")
           @output = @output + output + "\nBuild failed... Unable to retry.\n" 
-          File.write("#{@script_dir}/logs/#{@repo_name}_build.log", "log from command=#{cmd}\n#{output}", mode: "w")
+          File.write("#{@script_dir}/../logs/#{@repo_name}_build.log", "log from command=#{cmd}\n#{output}", mode: "w")
         else
           @spinner.success
         end
@@ -46,7 +60,7 @@ class ImageBuilder
         build_image
       else
         @output = @output + output + "\nBuild failed... Unable to retry.\n" 
-        File.write("#{@script_dir}/logs/#{@repo_name}_build.log", "log from command=#{cmd}\n#{output}", mode: "w")
+        File.write("#{@script_dir}/../logs/#{@repo_name}_build.log", "log from command=#{cmd}\n#{output}", mode: "w")
         @spinner.error(" - see #{@script_dir}/logs/#{@repo_name}_build.log")
         @success = false
       end
@@ -65,6 +79,20 @@ class ImageBuilder
   
     def success?
       return @success
+    end
+
+    def insert_maven_local_copy_in_dockerfile(dockerfile_path, m2_dir)
+      output = ""
+      dockerfile = File.new(dockerfile_path)
+      dockerfile.each do |line|
+        if line.start_with? 'WORKDIR' # We need the maven local repo available in each stage of the build
+          output << line
+          output << "COPY #{m2_dir}/ /root/.m2/repository/uk/gov/ida/\n"
+        else
+          output << line
+        end
+      end
+      output
     end
   end
   
